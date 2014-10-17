5749ced695d7a2877c392966f87abe2065e405a7
armandDu <dupuis.armand42@gmail.com>
2014-10-17 09:53:15 +0200

    Update Server tests

--- Server/src/test/java/com/github/armanddu/jaxws/example/AppTest.java
+++ Server/src/test/java/com/github/armanddu/jaxws/example/AppTest.java
@@ -1,5 +1,12 @@
 package com.github.armanddu.jaxws.example;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Endpoint;
+import javax.xml.ws.Service;
+
 import com.github.armanddu.jaxws.example.App;
 import com.github.armanddu.jaxws.example.AppImpl;
 
@@ -10,19 +17,23 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
+public class AppTest extends TestCase{
+
+	private static final String	PACKAGE_URL	= "http://example.jaxws.armanddu.github.com/";
+	private static final String	SERVICE_URL	= "http://localhost:4242/WS/App";
+
+	App							app;
+	Endpoint					endPoint;
+
 	/**
 	 * Create the test case
 	 *
 	 * @param testName
 	 *            name of the test case
+	 * @throws MalformedURLException 
 	 */
-	App	app;
-
 	public AppTest(String testName) {
 		super(testName);
-
-		app = new AppImpl();
 	}
 
 	/**
@@ -32,10 +43,23 @@ public class AppTest extends TestCase {
 		return new TestSuite(AppTest.class);
 	}
 
+	public void setUp() throws MalformedURLException {
+
+		endPoint = Endpoint.publish(SERVICE_URL, new AppImpl());
+		assertTrue((endPoint.isPublished()));
+
+		URL wsdlLocation = new URL(SERVICE_URL + "?wsdl");
+		QName servineQName = new QName(PACKAGE_URL, "AppImplService");
+		QName portQName = new QName(PACKAGE_URL, "AppImplPort");
+
+		Service service = Service.create(wsdlLocation, servineQName);
+		app = service.getPort(portQName, App.class);
+	}
+
 	/**
 	 * Rigourous Test :-)
 	 */
-	public void testApp() {
+	public void testHello() {
 		assertEquals("Hello John Doe !", app.grettings("John Doe"));
 	}
 
@@ -52,4 +76,10 @@ public class AppTest extends TestCase {
 			}
 		}
 	}
+
+	public void tearDown() {
+		endPoint.stop();
+		assertFalse(endPoint.isPublished());
+	}
+
 }
